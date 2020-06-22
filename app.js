ea3c08c6a6c45161251a09b7d240c5218e8fc7a1
jenjones319 <jenjones605@gmail.com>
2020-06-22 10:16:16 -0500

    problem 1 pass

--- app.js
+++ app.js
@@ -45,10 +45,10 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-        var theSum = a + b + c;
-        var messageOne = + a + 'and' + b + 'and' + c + 'sum to' + theSum + '.';
-        var theProduct = a * b * c;
-        var messageTwo = 'The product of' + a + 'and' + b + 'and' + c + 'is' + theProduct + '.';
+        var theSum = sum(a, sum(b,c)[0])[0];
+        var messageOne =a + ' and ' + b + ' and ' + c + ' sum to ' + theSum + '.git';
+        var theProduct = multiply(a, multiply(b,c)[0])[0];
+        var messageTwo ='The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + theProduct + '.';
         return [theSum, theProduct, messageOne, messageTwo];
       }
 
@@ -62,13 +62,14 @@ testSumAndMultiply(4,7,5);
 Write a function called sumArray() that takes in an array of numbers as its single argument and then returns an array where the first element is the sum of the numbers in the array, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
 "2,3,4 was passed in as an array of numbers, and 9 is their sum."
 IMPORTANT DETAIL: You may not use the arithmetic operator + in this function. To do addition, use your sum() function that you've already created. You're going to have to be resourceful to figure out how to do this. However, you may continue to use the + operator for string concatenation.
-Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumArray() function and see if the test passes.*/
+Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumArray() function and see if the test passes.*/gi
 
 // Write your code here
 var testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
-
+  var sumArray = sum(sumArr[0], sumArr[1], sumArr[2][0])[0];
+  var message = sumArr + ' was passed in as an array of numbers, and 9 is their sum.'
 }
 
 // Here is the test for sumArray(); uncomment it to run it
